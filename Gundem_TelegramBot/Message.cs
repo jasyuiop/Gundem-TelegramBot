7766e143c785cc23a8feaf9857d2951ebbb094aa
M.Emre Nefesli <emrenefesli@outlook.com>
2019-08-05 13:01:58 +0300

    Eski komutlar kaldırıldı, XRP adresi kaldırıldı, textler düzenlendi, debe kodu blok olarak yukarıya alındı
    Bot üzerinden komut bilgileri güncellendiği için ve kullanıcıların artık yeni komutların farkında olduğunu düşündüğüm için eski komutlardaki bilgilendirme mesajlarını kaldırıyorum.
    Bağış için yazdığım xrp adresimi kaldırıyorum.

--- Gundem_TelegramBot/Message.cs
+++ Gundem_TelegramBot/Message.cs
@@ -18,15 +18,14 @@ namespace Gundem_TelegramBot
                 if (e.Message.Text == "/start")
                     await Program.botClient.SendTextMessageAsync(
                     chatId: e.Message.Chat,
-                    text: "Merhaba, Ekşi sözlük gündemini, dünün en beğenilen entry'lerini, bana verilen entry numarasından entry'i sana gösterebilirim\nKullanabileceğin komutlar işte burada\n/yardim\n/gundem\n/entry\n/debe\n\n[Ben artık açık kaynak bir projeyim, tıkla ve github üzerinde bana gözat.](https://github.com/jasyuiop/Gundem-TelegramBot)\n\nGeliştiriciye destek olmak için;\nRipple XRP Adress =\nrDrwceWscNExnTmgxz51cRcrs24dhVEz3V\nXRP Tag = 0",
+                    text: "Merhaba, Ekşi sözlük gündemini, dünün en beğenilen entry'lerini, bana verilen entry numarasından entry'i sana gösterebilirim\nKullanabileceğin komutlar işte burada\n/gundem\n/debe\n/entry\n/yardim\n\n[Açık kaynak bir proje olduğumu biliyormusun? tıkla ve github üzerinde bana gözat](https://github.com/jasyuiop/Gundem-TelegramBot)",
                     parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown
                     );
-
 
                 else if (e.Message.Text == "/yardim")
                     await Program.botClient.SendTextMessageAsync(
                     chatId: e.Message.Chat,
-                    text: "Merhaba, Ekşi sözlük gündemini, dünün en beğenilen entry'lerini, bana verilen entry numarasından entry'i sana gösterebilirim\nKullanabileceğin komutlar işte burada\n/gundem\n/entry\n/debe\n\n[Ben artık açık kaynak bir projeyim, tıkla ve github üzerinde bana gözat.](https://github.com/jasyuiop/Gundem-TelegramBot)\n\nGeliştiriciye destek olmak için;\nRipple XRP Adress =\nrDrwceWscNExnTmgxz51cRcrs24dhVEz3V\nXRP Tag = 0",
+                    text: "Merhaba, Ekşi sözlük gündemini, dünün en beğenilen entry'lerini, bana verilen entry numarasından entry'i sana gösterebilirim\nKullanabileceğin komutlar işte burada\n/gundem\n/debe\n/entry\n\n[Açık kaynak bir proje olduğumu biliyormusun? tıkla ve github üzerinde bana gözat](https://github.com/jasyuiop/Gundem-TelegramBot)",
                     parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown
                     );
 
@@ -65,6 +64,46 @@ namespace Gundem_TelegramBot
                     chatId: e.Message.Chat,
                     // gelen veride boş satırlar olabiliyor bu yüzden onları Regex kullanarak temizliyorum
                     text: Regex.Replace(Data, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline),
+                    parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown
+                    );
+                }
+
+                else if (e.Message.Text == "/debe")
+                {
+                    Parsing parsed = new Parsing();
+                    HtmlDocument hookedDocument = parsed.HookSite("https://sozlock.com/");
+                    string xpath = @"/html/body/main/div/div[2]/ul";
+                    var returnedS_hList = parsed.TagData(hookedDocument, xpath);
+
+                    StringBuilder sbuilder = new StringBuilder();
+
+                    foreach (var item in returnedS_hList)
+                    {
+                        foreach (var Inner_li in item.SelectNodes("li"))
+                        {
+                            Queue<string> links = new Queue<string>();
+                            foreach (var Inner_divA in Inner_li.SelectNodes("div[2]//a"))
+                            {
+                                HtmlAttribute href_att = Inner_divA.Attributes["href"];
+                                links.Enqueue(href_att.Value);
+                            }
+                            foreach (var Inner_h3 in Inner_li.SelectNodes("h3"))
+                            {
+                                if (!string.IsNullOrEmpty(Inner_h3.ToString()))
+                                {
+                                    /* h3'den gelen text'in içerisinde kesme işaretleri decode edilmemiş olarak dönüyor.
+                                    bu yüzden kesme işareti içeren cümlelerde kesme işareti değil de "&#39" yazısı görünüyor.
+                                    bunu önlemek için HtmlDecode kullanıyorum
+                                    */
+                                    sbuilder.AppendLine("[" + System.Web.HttpUtility.HtmlDecode(Inner_h3.InnerText) + "]" + "(" + links.Dequeue() + ")");
+                                }
+                            }
+                        }
+                    }
+                    string Data = sbuilder.ToString();
+                    await Program.botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text: Regex.Replace(Data, @"^\s+$[\r\s]*", string.Empty, RegexOptions.Multiline),
                     parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown
                     );
                 }
@@ -122,76 +161,6 @@ namespace Gundem_TelegramBot
                         text: "Üzgünüm, geçerli bir entry numarası girmedin!\nEntry numarasını kontrol edip tekrar denersen sana yardımcı olmaya çalışacağım.\nUnutma entry numaraları sadece rakamlardan oluşur, harf ve karakter içermez."
                         );
                     }
-                }
-
-                else if (e.Message.Text == "/debe")
-                {
-                    Parsing parsed = new Parsing();
-                    HtmlDocument hookedDocument = parsed.HookSite("https://sozlock.com/");
-                    string xpath = @"/html/body/main/div/div[2]/ul";
-                    var returnedS_hList = parsed.TagData(hookedDocument, xpath);
-
-                    StringBuilder sbuilder = new StringBuilder();
-
-                    foreach (var item in returnedS_hList)
-                    {
-                        foreach (var Inner_li in item.SelectNodes("li"))
-                        {
-                            Queue<string> links = new Queue<string>();
-                            foreach (var Inner_divA in Inner_li.SelectNodes("div[2]//a"))
-                            {
-                                HtmlAttribute href_att = Inner_divA.Attributes["href"];
-                                links.Enqueue(href_att.Value);
-                            }
-                            foreach (var Inner_h3 in Inner_li.SelectNodes("h3"))
-                            {
-                                if (!string.IsNullOrEmpty(Inner_h3.ToString()))
-                                {
-                                    /* h3'den gelen text'in içerisinde kesme işaretleri decode edilmemiş olarak dönüyor.
-                                    bu yüzden kesme işareti içeren cümlelerde kesme işareti değil de "&#39" yazısı görünüyor.
-                                    bunu önlemek için HtmlDecode kullanıyorum
-                                    */
-                                    sbuilder.AppendLine("[" + System.Web.HttpUtility.HtmlDecode(Inner_h3.InnerText) + "]" + "(" + links.Dequeue() + ")");
-                                }
-                            }
-                        }
-                    }
-                    string Data = sbuilder.ToString();
-                    await Program.botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: Regex.Replace(Data, @"^\s+$[\r\s]*", string.Empty, RegexOptions.Multiline),
-                    parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown
-                    );
-                }
-
-                else if (e.Message.Text == "/eksigundemlink")
-                {
-                    await Program.botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Linkler başlık içerisine gömüldü, artık /gundem yazarak başlıklara tıklayınca direkt link'e gidebileceksiniz :)"
-                    );
-                }
-
-                else if (e.Message.Text == "/eksigundem")
-                {
-                    await Program.botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Linkler ve başlık /gundem 'de birleşti, artık linkleri ve başlıkları tek bir komut'ta görebileceksiniz :)"
-                    );
-                }
-                else if (e.Message.Text == "/help")
-                {
-                    await Program.botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Yardım almak için /yardim komutunu kullanmanı tavsiye ediyorum, geliştiricim artık böylesini uygun görüyor."
-                    );
-                }
-                else if (e.Message.Text == "/eksientry")
-                {
-                    await Program.botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Entry'leri görmek için /entry komutunu kullanmanı tavsiye ediyorum, artık başlık ismini ve başlık ismine gömülü entry link'ini de gösterebiliyorum."
-                    );
                 }
 
                 else
